173d6086b11307298614dd27e9579d19fe153923
Alex Caloggero <a.calog@protonmail.com>
2020-10-16 20:23:11 -0400

    Welcome page style update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,6 @@
 
 @section('content')
     <div class="container-fluid">
-        <h1>ThrillGIFS</h1>
         <div class="gallery__container gallery__outer-container">
             @include('modules.gallery')
         </div>

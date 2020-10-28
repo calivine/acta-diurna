b2eb0851e3360bd9af250225024b66c607f32448
Alex Caloggero <a.calog@protonmail.com>
2020-10-28 16:05:32 -0400

    Image centering

--- resources/views/content/watchers.blade.php
+++ resources/views/content/watchers.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-md-8">
+            <div class="col-md-9">
                 <section>
                     <hgroup>
                         <h1 class="post__title">657 Boulevard, Westfield, NJ 07901</h1>

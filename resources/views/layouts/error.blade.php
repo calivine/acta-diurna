08c704d19d7cea575c60ea6ceb02ee1051c83638
Alex Caloggero <a.calog@protonmail.com>
2020-10-31 14:27:56 -0400

    Errors layout

--- resources/views/layouts/error.blade.php
+++ resources/views/layouts/error.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.master')
 
-@section('title', '{{ $status_code }}')
+@section('title', $status_code)
 
 @section('content')
     <main class="container">
         <div class="row">
-            <div class="col-lg-4">
+            <div class="col-lg-9">
                 <h1>Error: {{ $status_code }}</h1>
             </div>
         </div>

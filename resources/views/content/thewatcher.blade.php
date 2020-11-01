c7110dcfffdf6c8ca444c5895b07ee02ba306da5
Alex Caloggero <a.calog@protonmail.com>
2020-11-01 01:29:17 -0500

    Added date

--- resources/views/content/thewatcher.blade.php
+++ resources/views/content/thewatcher.blade.php
@@ -4,6 +4,7 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-9">
+                <p class="post__date">October 31, 2020</p>
                 <section>
                     <hgroup>
                         <h1 class="post__title">657 Boulevard Westfield, NJ</h1>

ea01ae735cb1a7defa5be3831292f7ad31e15d59
Alex Caloggero <a.calog@protonmail.com>
2022-07-19 11:40:11 -0400

    Descending list of episodes

--- app/Http/Controllers/Content/GuestController.php
+++ app/Http/Controllers/Content/GuestController.php
@@ -79,7 +79,7 @@ class GuestController extends Controller
 
     public function getPodcasts()
     {
-        $podcasts = Podcast::all();
+        $podcasts = Podcast::all()->sortByDesc('id');
 
         return view('content.podcast.directory')->with(['podcasts' => $podcasts]);
     }

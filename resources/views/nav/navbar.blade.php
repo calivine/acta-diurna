0503d3dbeb9443ea735f747d9f6dcdef799d3d39
Alex Caloggero <a.calog@protonmail.com>
2022-08-11 14:23:29 -0400

    Navbar style update: text color & spacing

--- resources/views/nav/navbar.blade.php
+++ resources/views/nav/navbar.blade.php
@@ -1,15 +1,16 @@
-<nav class="navbar navbar-expand-md navbar-lite bg-white" id="nav">
+<nav class="navbar navbar-lite bg-light" id="nav">
+
     <!-- Left Side Of Navbar -->
     <a class="navbar-brand" href="{{ url('/') }}">{{ __('Nightmare Houses') }}</a>
 
     <!-- Right Side Of Navbar -->
     <!-- Nav links if User is authenticated -->
-    <ul class="nav justify-content-end">
+    <ul class="nav">
         @auth
             @isAdmin
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ url('/panel') }}">{{ __('Admin Panel') }}</a>
-                </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{ url('/panel') }}">{{ __('Admin Panel') }}</a>
+            </li>
             @endisAdmin
             <!-- Logout -->
             <li class="nav-item">

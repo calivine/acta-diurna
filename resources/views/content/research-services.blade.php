fe515f5752180d590bff560856e6c2462426db23
Alex Caloggero <a.calog@protonmail.com>
2023-08-08 10:48:14 -0400

    Add the pages

---
+++ resources/views/content/research-services.blade.php
@@ -0,0 +1,66 @@
+@extends('layouts.master')
+
+@section('title', 'The Watcher | Research Services')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <h2>Research for Hire - Exploring Nightmare Houses</h2>
+    </div>
+    <div class="row justify-content-center">
+        <div class="col-lg-8">
+            <p>Are you fascinated by the intriguing stories hidden within a haunted house? Do you want to uncover the history and mysteries of a home you’ve always been intrigued by?</p>
+
+            <p>Kate offers specialized research services tailored to reveal the secrets of these unique and historic homes.</p>
+        </div>
+        
+    </div>
+    <div class="row justify-content-center">
+        <h2>Why Choose Kate?</h2>
+    </div>
+    <div class="row justify-content-center">
+        
+        <div class="col-lg-8">
+            <p>Kate is passionate about exploring the world of haunted buildings. She is an experienced researcher and equipped with the knowledge and expertise to shed light on the captivating narratives of Nightmare Houses, making history come alive.</p>
+        </div>
+    </div>
+    <div class="row justify-content-center">
+        <h2>Services:</h2>
+    </div>
+    <div class="row justify-content-center">
+        
+        <div class="col-lg-8">
+            <ul>
+                <li><strong>Historical Research:</strong>Unearth the forgotten people, stories and events that shaped a nightmare house. Kate’s meticulous research dives into the past, revealing the rich history behind these intriguing places.
+                </li>
+                <li><strong>Architectural Analysis:</strong>  Discover the unique features and design of the home. Kates explores the architectural details that give these houses their distinct character.
+                </li>
+                <li><strong>Paranormal Investigations:</strong> If you're curious about unexplained phenomena, Kate can arrange for an investigation into the mysterious occurrences related to a nightmare house. Kate approaches these inquiries with a mix of scientific inquiry and open-mindedness.
+                </li>
+            </ul>
+        </div>
+    </div>
+    <div class="row justify-content-center">
+        <h2>How It Works</h2>
+    </div>
+    <div class="row justify-content-center">
+        <div class="col-lg-8">
+            <ul>
+                <li><strong>Consultation:</strong>Schedule a meeting with Kate to discuss specific interests and research needs related to your nightmare house. Share any stories or legends you've heard about these fascinating structures as well as the primary address and other known information.</li>
+                <li><strong>Customized Proposal:</strong>Based on your consultation, Kate will create a personalized research proposal. This will outline our plan, methodology, estimated timeline, and cost.</li>
+                <li><strong>In-Depth Research:</strong> Once the proposal is approved, Kate will delve into the history and mysteries of the nightmare house. Kate’s research will delve into historical archives and explore unique architectural features.</li>
+                <li><strong>Fascinating Discoveries:</strong> You'll receive a comprehensive research report, filled with interesting findings and insights about the intriguing world of the nightmare house.</li>
+            </ul>
+        </div>
+    </div>
+    <div class="row justify-content-center">
+        <h2>Contact</h2>
+    </div>
+    <div class="row justify-content-center">
+        <div class="col-lg-8"><p>If you're ready to embark on a journey into the secrets of a nightmare house, feel free to reach out at nightmarehousespod@gmail.com. Together, let's uncover the fascinating stories behind these captivating structures.</p>
+            <p>Discover the history of a Nightmare House you want to learn more about.</p></div>
+        
+    </div>
+</div>
+
+@endsection
